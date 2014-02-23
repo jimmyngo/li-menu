149978bfbe14422f27887a3a337fbdd531bd63d6
Bryce J <jasmerb@gmail.com>
2014-02-23 09:38:33 -0800

    Adding a setup.py so that this can be installed somewhere in $PATH.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='li-menu',
+      version='1.1',
+      description='Display LinkedIn menu from the command line',
+      author='Jimmy Ngo',
+      author_email='jimmyngo@example.com',
+      url='http://github.com/jimmyngo/li-menu',
+      scripts=['menu.py'],
+     )
